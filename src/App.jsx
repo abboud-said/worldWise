edab9a34b9c18b2eea9d193fd56b0d295f9e1302
abboud-said <abboud.said@hotmail.com>
2026-04-02 21:41:17 +0200

    State In URL
    Put state in URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
@@ -9,6 +9,9 @@ import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+
 
 
 const BASE_URL = "http://localhost:8000";
@@ -44,10 +47,14 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
         
-        <Route index element={<CityList cities={cities} loading={loading} />} />
+        <Route index element={<Navigate replaceto="cities" />} />
         <Route path="Cities" element={<CityList cities={cities} loading={loading} />} /> 
+
+          <Route path="Cities/:id" element={<City />} /> 
+
+
          <Route path="Countries" element={<CountryList cities={cities} loading={loading} />} /> 
-        <Route path="from" element={<p>From</p>} />
+        <Route path="from" element={<Form />} />
        
 
         </Route>
